462782b026d2a93d980eb5f96856904184dc5970
Godwin Umosen <umosengodwin568@gmail.com>
2025-04-28 10:35:37 +0100

    Third commit

--- deus/settings.py
+++ deus/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
 # Password validatio
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
